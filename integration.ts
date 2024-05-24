cb3caec3ceb86975bf104fd77dad958278368ec8
Yash Chhatre <ychhatre@gmail.com>
2024-05-24 10:30:33 -0700

    minor fix with random timing

--- integration.ts
+++ integration.ts
@@ -1,5 +1,5 @@
 import Client from "./client"; 
 
 
-const client = new Client("http://localhost:3000", 1.5)
+const client = new Client("http://localhost:3000", 1.25)
 client.complete(1000, 5000)
--- server.ts
+++ server.ts
@@ -13,15 +13,16 @@ server.listen(PORT, () => {
 const startTime = Date.now();
 
 // approx time for video --> Math.random() used to simulate various different videos of sizes and lengths
-const VIDEO_THRESHOLD = Math.random() * 1000; 
+const random = Math.random()
+console.log(random)
+const VIDEO_THRESHOLD = random * 10000; 
 
-server.get("/status/:userID/", (req, res) => {
-  console.log(req.params.userID);
+server.get("/status", (req, res) => {
   const timeElapsed = Date.now() - startTime;
 
   if (timeElapsed < VIDEO_THRESHOLD) { // if video is still loading
     return res.status(202).json({ status: "pending" });
-  } else if (Math.random() < 0.35) { // if there is an error while the video is loading (Math.random() used to simulate random errors)
+  } else if (Math.random() < 0.10) { // if there is an error while the video is loading (Math.random() used to simulate random errors)
     return res.status(502).json({ status: "error" });
   } else if (timeElapsed > VIDEO_THRESHOLD) { // video fully loaded 
     return res.status(200).json({ status: "accepted" });
